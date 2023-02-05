3b75a552c4952c30c6aef433310f8744a2eaadf1
Alex Kuzmin <hrumos@gmail.com>
2023-02-05 21:58:43 +0100

    Add basic file uploading functionality

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,10 +6,13 @@ use App\Entity\User;
 use App\Form\UserType;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 /**
  * @Route("/user")
@@ -20,19 +23,50 @@ class UserController extends AbstractController
      * @Route("/edit/{id}", name="app_user_edit", methods={"GET", "POST"})
      */
     public function edit(Request $request, User $user, UserRepository $userRepository,
-                         UserPasswordHasherInterface $userPasswordHasher): Response
+                         UserPasswordHasherInterface $userPasswordHasher, SluggerInterface $slugger): Response
     {
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-                // encode the plain password
-                $user->setPassword(
-                    $userPasswordHasher->hashPassword(
-                        $user,
-                        $form->get('password')->getData()
-                    )
-                );
+            /** @var UploadedFile $avatarFile */
+            $avatarFile = $form->get('avatar')->getData();
+            // this condition is needed because the 'avatar' field is not required
+            // so the PDF file must be processed only when a file is uploaded
+            if ($avatarFile) {
+                $originalFilename = pathinfo($avatarFile->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $avatarFile->guessExtension();
+
+                if (!file_exists($this->getParameter('avatars_directory'))) {
+                    return new Response('Failed to upload image: ' . $this->getParameter('avatars_directory'). ' does not exist');
+                }
+
+                if (!is_writable($this->getParameter('avatars_directory'))) {
+                    return new Response('Failed to upload image: ' . $this->getParameter('avatars_directory'). ' is not writable');
+                }
+
+                // Move the file to the directory where avatars are stored
+                try {
+                    $avatarFile->move(
+                        $this->getParameter('avatars_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    return new Response('Failed to upload image: ' . $e->getMessage());
+                    // ... handle exception if something happens during file upload
+                }
+
+                // updates the 'avatarFilename' property to store the PDF file name
+                // instead of its contents
+                $user->setAvatarFileName($newFilename);
+            }
+
+            // encode the plain password
+            $user->setPassword(
+                $userPasswordHasher->hashPassword($user,$form->get('password')->getData())
+            );
 
             $userRepository->add($user, true);
 
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -5,18 +5,51 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
 
 class UserType extends AbstractType
 {
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     * @throws \TypeError
+     */
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('username')
             ->add('password', PasswordType::class)
             ->add('email', EmailType::class)
+            ->add('avatar', FileType::class, [
+                'label' => 'Avatar (image file)',
+
+                // unmapped means that this field is not associated to any entity property
+                'mapped' => false,
+
+                // make it optional so you don't have to re-upload the PDF file
+                // every time you edit the Product details
+                'required' => false,
+
+                // unmapped fields can't define their validation using annotations
+                // in the associated entity, so you can use the PHP constraint classes
+                'constraints' => [
+                    new File([
+                        'maxSize' => '5024k',
+                        'mimeTypes' => [
+                            'image/gif',
+                            'image/jpeg',
+                            'image/png',
+                            'image/gif',
+                            'image/x-png',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid image',
+                    ])
+                ],
+            ])
         ;
     }
 
