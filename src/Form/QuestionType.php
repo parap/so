1e335c908d07aabb9399543c0459a0fed41f7719
Alex Kuzmin <hrumos@gmail.com>
2023-02-05 16:05:13 +0100

    Make question field textarea instead of textfield

--- src/Form/QuestionType.php
+++ src/Form/QuestionType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Question;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +13,12 @@ class QuestionType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('content')
+            ->add('content', TextareaType::class, [
+                'attr' => [
+                    'cols' => 60,
+                    'rows' => 5
+                ]
+            ])
         ;
     }
 
