7010b33638acaa8f9e9c32ce60730280073d8a58
Alex Kuzmin <hrumos@gmail.com>
2023-02-05 18:12:12 +0100

    Add migration to create rating DB table

---
+++ migrations/Version20230205171041.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230205171041 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE rating (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, answer_id INT NOT NULL, value SMALLINT NOT NULL, INDEX IDX_D8892622A76ED395 (user_id), INDEX IDX_D8892622AA334807 (answer_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE rating ADD CONSTRAINT FK_D8892622A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE rating ADD CONSTRAINT FK_D8892622AA334807 FOREIGN KEY (answer_id) REFERENCES answer (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE rating DROP FOREIGN KEY FK_D8892622A76ED395');
+        $this->addSql('ALTER TABLE rating DROP FOREIGN KEY FK_D8892622AA334807');
+        $this->addSql('DROP TABLE rating');
+    }
+}
